3b0b464b9bd88c6b3ecbf3ae92a220d16fa3015c
Madison <madivallarino@gmail.com>
2021-09-26 17:27:56 -0400

    commit tree f40ffab2364c77e8e47abf33ef237e9c1b316c09

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+class Student < User
+    # attr_accessor :knowledge
+    def initialize ()
+        @knowledge = []
+    end
+
+    def knowledge
+        @knowledge
+    end
+
+    def learn(string)
+
+        @knowledge << string
+
+        #my crazy method is below
+        # new_knowledge = string.split('')
+        # self.knowledge += new_knowledge
+    end
 
 end
